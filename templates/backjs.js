3d0adf3e7b7370e0b984ee3800523da4d7e8bc4e
msisir2006-ship-it <msisir2006@gmail.com>
2025-12-24 11:10:32 +0530

    Add files via upload

---
+++ templates/backjs.js
@@ -0,0 +1,5 @@
+// NOTE: This file was moved to `static/backjs.js` and updated to be safe on pages
+// where the sign in/sign up elements might not exist. Keep this placeholder to avoid
+// accidental usage of the old file location.
+
+/* If you want the active implementation, use: static/backjs.js */
